6fd9c9e13ecf17f505876b7dab7fd14f8be180ad
Muhammad Shahwal <130134544+sawalreverr@users.noreply.github.com>
2023-10-20 00:56:01 +0700

    Update httphelper.dart

--- lib/helper/httphelper.dart
+++ lib/helper/httphelper.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 import 'package:http/http.dart' as http;
 
 class httpHelper {
-  final String _urlKey = "?api_key=18b89a60063c48afafb7248665905811";
+  final String _urlKey = "?api_key=?";
   final String _urlBase = "https://api.themoviedb.org/";
 
   Future<String> getMovie(String film) async {
